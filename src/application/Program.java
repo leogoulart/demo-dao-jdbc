9856497aaf1dbd661059877e206c62b7d2b4c667
leogoulart <leogoulart0911@gmail.com>
2020-12-17 09:50:02 -0300

    Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Hardware");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(2, "Jack Mack Flack", "homemdepreto@gmail.com", new Date(), 3200.0, obj);
+		
+		System.out.println(seller);
 		
 	}
 	
